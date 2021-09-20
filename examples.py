6d2aa0bcec80788bb9053ffabe1c5e0ab383ee6e
indy <indymccarthy@gmail.com>
2021-09-20 08:53:07 +0200

    :art: add comment

--- examples.py
+++ examples.py
@@ -1,5 +1,6 @@
-if __name__ == '__main__':
+#if __name__ == '__main__':
 
+# Dictionnaries
 phonebook = {
     "bob": 7387,
     "alice": 3719,
@@ -8,9 +9,11 @@ phonebook = {
 
 squares = {x: x * x for x in range(6)}
 
+# OrderedDict
 import collections
 d = collections.OrderedDict(one=1, two=2, three=3)
 
+# defaultdict
 from collections import defaultdict
 dd = defaultdict(list)
 
@@ -18,8 +21,10 @@ dd = defaultdict(list)
 # initializes it using the default factory,
 # i.e. list() in this example:
 dd["dogs"].append("Rufus")
+# A new list containing "Rufus" is created and associated to the key "dogs"
 dd["dogs"].append("Kathrin")
 
+# ChainMap
 # concat multiple dicts
 # chain contient les pointeurs vers les dicts
 # les dicts peuvent être modifié et pris en
@@ -29,20 +34,21 @@ dict1 = {"one": 1, "two": 2}
 dict2 = {"three": 3, "four": 4}
 chain = ChainMap(dict1, dict2)
 dict1["five"]=5
-chain["five"]
+print(chain["five"])
 
+# Read only dict
 from types import MappingProxyType
 read_only = MappingProxyType(dict1)
 
-# Tableau de float
+# Tableau typé de float
 import array
 arr = array.array("f", (1.0, 1.5, 2.0, 2.5))
 
 # Immutable array of single bytes < 256
 arr = bytes((0, 1, 2, 3))
-arr[1]
+print(arr[1])
 
-# Mutable
+# Mutable array of single bytes
 arr = bytearray((0, 1, 2, 3))
 
 
@@ -53,12 +59,16 @@ a = array(l)
 print(a)
 print(a.shape)
 print(a.dtype)
+# [1. 2. 3.]
+# (3,)
+# float64
 
 # Création d'une class avec attributs typés
 from dataclasses import dataclass, fields
 from pydantic import validate_arguments
 
-
+# Validate argument allows to pass numeric as String and understand cast True as 1.0
+# It throws an error if mileage="aaa"
 @validate_arguments
 @dataclass
 class Car:
@@ -75,7 +85,9 @@ class Car:
                                  f'got {repr(value)}')
 
 
-
+# Counter allows to aggregate dict with integer as value
+# Also allows to count char in a string : c = Counter('abcdeabcdabcaba')
+# c.most_common(3)
 from collections import Counter
 inventory = Counter()
 
@@ -87,3 +99,4 @@ inventory
 more_loot = {"sword": 1, "apple": 1}
 inventory.update(more_loot)
 #inventory
+#Counter({'bread': 3, 'sword': 2, 'apple': 1})
